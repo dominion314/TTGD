9be074b38b29e20a07dd6e75c7170001aa9e6ae6
dominion314 <dominickhrndz314@gmail.com>
2021-12-30 21:58:24 -0600

    update

--- split.py
+++ split.py
@@ -5,7 +5,7 @@ def removeSJW(mystring):
     else:
      return mystring.replace("SJW","") 
 
-# expected output: WGU Rocks
+
 print(removeSJW('SJW is term used to describe a generation of people who like to scream at their mom in Walmart.'))
-# expected output: Hello, John
+
 print(removeSJW('Wouldnt it be niceSJW if we could remove the word completely, SJWit seems to sneakSJW in wherever it can, only to contaminate logical discourse and adult SJWconversations.'))
