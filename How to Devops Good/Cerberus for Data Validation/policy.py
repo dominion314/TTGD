f353677d76ca6efb5297b6040a05b6d9bcd37892
dominion314 <dominickhrndz314@gmail.com>
2022-01-01 14:11:16 -0600

    Update to script

--- How to Devops Good/Cerberus for Data Validation/policy.py
+++ How to Devops Good/Cerberus for Data Validation/policy.py
@@ -1,4 +1,4 @@
-"""  Simple Cerberus Validator Script """
+"""  Cerberus Schema Enforcer """
 from cerberus import Validator #import the Cerberus library
 import yaml                    #import the yaml library 
 
@@ -8,13 +8,13 @@ with open('PSA_Customer_Request.yaml') as f: #open the customers PSA request
 with open('PSA_Schema_Template.yaml') as f: #open the PSA template
     SCHEMA = yaml.load(f, Loader=yaml.FullLoader) #We will call this info SCHEMA
 
-V = Validator() #This variable will use the function that compares the Customer Request to the Tempalate we create.
+V = Validator() #This variable will use the function that compares the Customer Request to the Schema Template.
 V.allow_unknown = True #This will allow unknown document key pairs.
 
 if V.validate(DATA, SCHEMA): #This will validate the DATA info against the Template Schema.
-    print('Pass') #If the DATA passes the Template, we will print pass.
+    print('Pass') #If the DATA passes the Template with no violations, we will print pass.
     print(V.document)
 else:
-    print('Fail') #If the DATA does not pass the schema check, we will print fail. 
+    print('Fail') #If the DATA does not pass the schema check and does find validation, we will print fail. 
     ERR = V.errors
     print(ERR)
